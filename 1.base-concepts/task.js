afcf45fe8d95321328da0cd45e4f27919f2ec677
NadezhdaEduardova <124979618+NadezhdaEduardova@users.noreply.github.com>
2023-02-13 18:12:58 +0300

    Update task.js
    task 1 quadratic equation

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,18 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
+   let discriminant = b*b - 4 * a * c;
   
+  if (discriminant === 0) {
+      let rootFirst = -b / (2 * a);
+          arr.push(rootFirst);
+    
+  } else if (discriminant > 0) {
+      let rootFirst = (-b + Math.sqrt(discriminant)) / (2 * a);
+          arr.push(rootFirst);
+      let rootSecond = (-b - Math.sqrt(discriminant)) / (2 * a);
+          arr.push(rootSecond);
+  } 
   return arr;
 }
 
