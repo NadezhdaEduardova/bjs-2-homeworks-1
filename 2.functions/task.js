545e310d874faa5a0838190fe34e014081b1074f
NadezhdaEduardova <124979618+NadezhdaEduardova@users.noreply.github.com>
2023-02-15 15:38:01 +0300

    Update task.js
    functin-task 1

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,8 +1,25 @@
+// Задача 1
 function getArrayParams(...arr) {
+  let mim = Infinity;
+  let max = Infinity;
+  let sum = 0;
+  let avg;
+  
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+    sum += arr[i];
+  }
+  avg = (sum / arr.length).toFixed(2);
 
   return { min: min, max: max, avg: avg };
 }
 
+// Задача 2
 function summElementsWorker(...arr) {
 
 }
