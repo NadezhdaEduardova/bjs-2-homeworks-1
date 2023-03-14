3dbe16240cf0b2dafb4f1532cc83fa49e73de2e4
NadezhdaEduardova <124979618+NadezhdaEduardova@users.noreply.github.com>
2023-03-14 19:04:21 +0300

    JS-04 objects

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,50 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+  if (this.hasOwnProperty('marks')) {
+    this.marks.push(...marks);
+  }
 }
 
 Student.prototype.getAverage = function () {
-  
+  let sum = 0;
+  let count = 0;
+  let average;
+  if (this.hasOwnProperty('marks') && this.marks.length > 0) {
+    for (let mark of this.marks) {
+      sum += mark;
+      count += 1;
+    };
+    average = sum / count;
+  } else {
+    average = 0;
+  }
+  return average;
 }
 
 Student.prototype.exclude = function (reason) {
-  
+    delete this.subject;
+    delete this.marks;
+    this.exclude = reason; 
 }
+
+let student1 = new Student("Василиса", "женский", 19);
+student1.setSubject("Algebra");
+console.log(student1.getAverage());
+student1.addMarks(2, 4, 6);
+console.log(student1.getAverage());
+console.log(student1);
+
+let student2 = new Student("Артём", "мужской", 25);
+student2.setSubject("Geometry");
+student2.exclude('плохая учёба')
+console.log(student2)
