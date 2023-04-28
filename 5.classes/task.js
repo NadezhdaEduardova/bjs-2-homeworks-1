fd0a06756b8a001f363acf0389b4aaf3d944cf6a
NadezhdaEduardova <124979618+NadezhdaEduardova@users.noreply.github.com>
2023-04-28 10:30:52 +0300

    Update task.js
    05 classes 1 и 2 задачи

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -1,8 +1,8 @@
-// Задача № 1 //
+// Задача 1 //
 class PrintEditionItem {  
   constructor(name, releaseDate, pagesCount) {
     this.name = name;
-    this.releaseDate = releasDate;
+    this.releaseDate = releaseDate;
     this.pagesCount = pagesCount;
     this.state = 100;
     this.type = null;
@@ -67,6 +67,70 @@ class DetectiveBook extends Book {
     this.type = "detective";
   }
 }
+
+// Задача 2//
+class Library {
+  constructor(name, books) {
+    this.name = name;
+    this.books = [];
+  }
+  
+  addBook(book) {
+    if (book.state > 30) {
+      this.books.push(book);
+    }
+  }
+    
+  findBookBy(type, value) {
+    for (let book in this.books) {
+      if (this.books[book][type] === value) {
+        return this.books[book];
+      }
+    }
+    return null;
+  }
+  
+  giveBookByName(bookName) {
+    for (let book in this.books) {
+      if (this.books[book].name === bookName) {
+        return this.books.splice(book, 1);
+      }
+    }
+    return null;
+  }
+}
+const library = new Library("Библиотека имени Ленина");
+
+library.addBook(
+ new DetectiveBook(
+   "Артур Конан Дойл",
+   "Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе",
+   2019,
+   1008
+ )
+);
+library.addBook(
+ new FantasticBook(
+   "Аркадий и Борис Стругацкие",
+   "Пикник на обочине",
+   1972,
+   168
+ )
+);
+library.addBook(new NovelBook("Герберт Уэллс", "Машина времени", 1895, 138));
+library.addBook(new Magazine("Мурзилка", 1924, 60));
+
+console.log(library.findBookBy("name", "Властелин колец"));
+console.log(library.findBookBy("releaseDate", 1924).name);
+
+console.log("Количество книг до выдачи: " + library.books.length);
+library.giveBookByName("Машина времени");
+console.log("Количество книг после выдачи: " + library.books.length);
+
+// Задача 3 //
+
+
+  
 
 
  
